0831e09abe4e7eb8ab1e96fdc78bbe093cb7bf99
Harsh-44 <92668529+Harsh-44@users.noreply.github.com>
2024-07-03 05:22:54 +0800

    Update shell.h

--- source/shell.h
+++ source/shell.h
@@ -4,7 +4,49 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <time.h>
+#include <sys/resource.h>
+#include <sys/time.h>
+/*
+Handler of each shell builtin function
+*/
+extern int enable_color;
 
+int shell_cd(char **args);
+int shell_help(char **args);
+int shell_exit(char **args);
+int shell_usage(char **args);
+int list_env(char **args);
+int set_env_var(char **args);
+int unset_env_var(char **args);
+int shell_browse(char **args);
+int toggle_color(char **args);
+void execute_cseshellrc();
+
+const char *builtin_commands[] = {
+    "cd",    // Changes the current directory of the shell to the specified path. If no path is given, it defaults to the user's home directory.
+    "help",  //  List all builtin commands in the shell
+    "exit",  // Exits the shell
+    "usage", // Provides a brief usage guide for the shell and its built-in command
+    "env", // Lists all the environment variables currently set in the shell
+    "setenv", // Sets or modifies an environment variable for this shell session
+    "unsetenv", // Removes an environment variable from the shell
+    "browse", // Opens website in default browser
+    "togglecolor" // Toggles colour between normal and aid colourblind personell
+    };
+    
+/*** This is array of functions, with argument char ***/
+int (*builtin_command_func[])(char **) = {
+    &shell_cd,     // builtin_command_func[0]: cd 
+    &shell_help,   // builtin_command_func[1]: help
+    &shell_exit,   // builtin_command_func[2]: exit
+    &shell_usage,  // builtin_command_func[3]: usage
+    &list_env,     // builtin_command_func[4]: env
+    &set_env_var,  // builtin_command_func[5]: setenv
+    &unset_env_var, // builtin_command_func[6]: unsetenv
+    &shell_browse,  // builtin_command_func[7]: browse
+    &toggle_color   // builtin_command_func[7]: unsetenv
+};
 
 #define MAX_LINE 1024
 #define MAX_ARGS 64
